e05748d33c8a6e060dbf975a660f65071d8bbf23
Bogdan Putineanu <94795061+197Bogdan@users.noreply.github.com>
2023-11-20 01:40:32 +0200

    drop key returns it to original position
    TO DO: only return the key if aiming at the key hanger
    + fixed multiple keys highlighting at once bug
    + added component tags system

--- EscapeHouseGit/Assets/Code/Scripts/PlayerInteractionsController.cs
+++ EscapeHouseGit/Assets/Code/Scripts/PlayerInteractionsController.cs
@@ -12,6 +12,10 @@ public class PlayerInteractionsController : MonoBehaviour
     [SerializeField] private float itemPickupDistance = 5f;
 
     private Transform pickedItem = null;
+    private Vector3 initialPosition;     // initial position of held item
+    private Quaternion initialRotation;   // initial rotation of held item
+    private Transform initialParent = null;          // initial parent of held item
+    
     private Transform targetedItem = null;
 
     void Update()
@@ -19,16 +23,22 @@ public class PlayerInteractionsController : MonoBehaviour
         RaycastHit hit;
         bool cast = Physics.Raycast(playerHead.position, playerHead.forward, out hit, itemPickupDistance);
 
-        if (cast && hit.transform.CompareTag("CanBePickedUp"))  // hovering interactable object
+        if (cast && hit.collider.gameObject.GetComponent<InteractableObjectTag>())  // hovering interactable object
         {
+            if(targetedItem != null && targetedItem != hit.transform && targetedItem.GetComponent<Outline>() != null)   // exit hovering previous interactable object
+                targetedItem.GetComponent<Outline>().enabled = false;
             targetedItem = hit.transform;
 
-            pickUpText.GetComponent<TMP_Text>().enabled = true;
+            
 
             if (hit.transform.GetComponent<Outline>() != null && pickedItem == null)
+            {
+                pickUpText.GetComponent<TMP_Text>().enabled = true;
                 hit.transform.GetComponent<Outline>().enabled = true;
+            }
+                
         }
-        else if (targetedItem != null)  // exit hovering interactable object
+        else if (targetedItem != null)  // if stopped hovering interactable object
         {
             pickUpText.GetComponent<TMP_Text>().enabled = false;
             
@@ -36,37 +46,49 @@ public class PlayerInteractionsController : MonoBehaviour
                 targetedItem.GetComponent<Outline>().enabled = false;
         }
 
+
+
         if (Input.GetKeyDown(KeyCode.F))    
         {
             if (pickedItem != null)     // drop held item
             {
-                pickedItem.SetParent(null);
 
-                if (pickedItem.GetComponent<Rigidbody>() != null)
-                    pickedItem.GetComponent<Rigidbody>().isKinematic = false;
+                Debug.Log(pickedItem.position);
+                Debug.Log(initialPosition);  
+                pickedItem.SetParent(initialParent);
+                Debug.Log(pickedItem.position);
+                pickedItem.position = initialPosition;
+                Debug.Log(pickedItem.position);
+                pickedItem.rotation = initialRotation;
 
-                if (pickedItem.GetComponent<Collider>() != null)
-                    pickedItem.GetComponent<Collider>().enabled = true;
+              //  if (pickedItem.GetComponent<Rigidbody>() != null)
+                //    pickedItem.GetComponent<Rigidbody>().isKinematic = false;
+
+             //   if (pickedItem.GetComponent<Collider>() != null
+              //      && pickedItem.GetComponent<Collider>().enabled == false)
+              //      pickedItem.GetComponent<Collider>().enabled = true;
 
                 pickedItem = null;
             }
             else if (cast)  // try picking item up
             {
-                Debug.Log(hit.transform.name);
 
-                if (hit.transform.CompareTag("CanBePickedUp"))
+                if (hit.collider.gameObject.GetComponent<PickableObjectTag>())
                 {
                     pickedItem = hit.transform;
+                    initialPosition = pickedItem.position;
+                    initialRotation = pickedItem.rotation;
+                    initialParent = hit.transform.parent;
 
                     pickedItem.SetParent(playerHand);         // setParent(camera) to follow camera
                     pickedItem.position = playerHand.position;
                     pickedItem.rotation = Quaternion.Euler(playerHand.rotation.eulerAngles + new Vector3(0f, -15f, 90f));   // align to hand + offset so object is facing forward
 
-                    if (pickedItem.GetComponent<Rigidbody>() != null)
-                        pickedItem.GetComponent<Rigidbody>().isKinematic = true;
+                   // if (pickedItem.GetComponent<Rigidbody>() != null)
+                     //   pickedItem.GetComponent<Rigidbody>().isKinematic = true;
 
-                    if (pickedItem.GetComponent<Collider>() != null)
-                        pickedItem.GetComponent<Collider>().enabled = false;
+                  //  if (pickedItem.GetComponent<Collider>() != null)
+                     //   pickedItem.GetComponent<Collider>().enabled = false;
                 }
             }
         }

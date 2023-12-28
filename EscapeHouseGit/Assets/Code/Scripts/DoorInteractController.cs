3239489f5eb490031815776bc2012d2df30d90e4
MafteiValentin <maftei_valentin06@yahoo.com>
2023-12-28 23:03:29 +0200

    Modified basement and others

---
+++ EscapeHouseGit/Assets/Code/Scripts/DoorInteractController.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class DoorInteractController : MonoBehaviour
+{
+    private bool _isAnimating = false;
+    private bool _isOpen = false;
+    private bool _isLocked = true;
+
+    private List<int> _usedKeys = new List<int>();
+    private readonly List<int> _correctKeys = new List<int> {2, 5, 6};
+
+    private PlayerInteractionsController _player = null;
+
+    [SerializeField]
+    public float maxInteractDistance = 5.0f;
+
+    [SerializeField]
+    private AudioSource _doorOpenSound = null;
+    private float _doorOpenSoundDelay = 0.0f;
+    [SerializeField]
+    private AudioSource _doorCloseSound = null;
+    [SerializeField]
+    private AudioSource _doorUnlockedSound = null;
+    [SerializeField]
+    private AudioSource _doorLockedSound = null;
+    [SerializeField]
+    private AudioSource _doorOpenWithCreakSound = null;
+
+
+    private void Start()
+    {
+        _player = FindObjectOfType<PlayerInteractionsController>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        bool cast = Physics.Raycast(_player.playerHead.position, _player.playerHead.forward, out hit, maxInteractDistance);
+
+        if (Input.GetKeyDown(KeyCode.F) && cast && hit.collider.gameObject.GetComponent<DoorTag>() && !_isAnimating)
+        {
+            if (_isOpen)
+                CloseDoor();
+            else
+            {
+                if (_isLocked)
+                    TryOpenDoor();
+                else
+                    OpenDoor();
+            }
+        }
+    }
+
+    void CloseDoor()
+    {
+        _usedKeys.Clear();
+        _isOpen = false;
+        StartCoroutine(RotateDoor(90, 1.0f));
+        _doorCloseSound.Play();
+    }
+
+    void OpenDoor()
+    {
+        _isOpen = true;
+        StartCoroutine(RotateDoor(0, 1.0f));
+        _doorOpenSound.Play();
+    }
+
+    void OpenDoorWithCreak()
+    {
+        _isOpen = true;
+        StartCoroutine(RotateDoor(0, 4.5f));
+        _doorOpenWithCreakSound.Play();
+    }
+
+    void TryOpenDoor()
+    {
+        
+        if (_player != null && _player.GetHeldKeyNumber() != -1)
+        {
+            int keyNumber = _player.GetHeldKeyNumber();
+            if (_usedKeys.Contains(keyNumber))
+            {
+                Debug.Log("You have already used this key!");
+                return;
+            }
+            if (_usedKeys.Count < 3)
+            {
+                Debug.Log("Used key " + keyNumber);
+
+                if (_usedKeys.Count < 2) // play key unlocking sound only for the first 2 keys
+                    _doorUnlockedSound.Play();
+
+                _usedKeys.Add(keyNumber);             
+            }
+            if (_usedKeys.Count == 3)
+            {
+                for(int index = 0; index < _usedKeys.Count; index++)
+                {
+                    if (_usedKeys[index] != _correctKeys[index])
+                    {
+                        _usedKeys.Clear();
+                        Debug.Log("Wrong key numbers or order!");
+                        _doorLockedSound.Play();
+                        return;
+                    }
+                }                                                    
+                _isLocked = false;
+                OpenDoorWithCreak();
+            }
+
+        }
+        else
+            _doorLockedSound.Play();
+
+    }
+
+    IEnumerator RotateDoor(float targetAngle, float rotationDuration)
+    {
+        float currentAngle = transform.eulerAngles.y;
+        float elapsedRotationTime = 0f;       
+        _isAnimating = true;
+
+        while (elapsedRotationTime < rotationDuration)
+        {
+            float newAngle = Mathf.LerpAngle(currentAngle, targetAngle, elapsedRotationTime / rotationDuration);
+            transform.eulerAngles = new Vector3(0, newAngle, 0);
+
+            elapsedRotationTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.eulerAngles = new Vector3(0, targetAngle, 0);
+        _isAnimating = false;
+    }
+}
---
+++ EscapeHouseGit/Assets/Code/Scripts/PlayerInteractionsController.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerInteractionsController : MonoBehaviour
+{
+    public Transform playerHead;
+    public Transform playerHand;
+
+    public TextMeshProUGUI pickUpText;
+    public TextMeshProUGUI interactText;
+    public TextMeshProUGUI putItemBackText;
+
+    [SerializeField]
+    private float itemPickupDistance = 5f;
+
+    private Transform pickedItem = null;
+    private Vector3 initialPosition;     // initial position of held item
+    private Quaternion initialRotation;   // initial rotation of held item
+    private Transform initialParent = null;          // initial parent of held item
+
+    private Transform targetedItem = null;
+
+    private void DisableTexts()
+    {
+        pickUpText.GetComponent<TMP_Text>().enabled = false;
+        interactText.GetComponent<TMP_Text>().enabled = false;
+        putItemBackText.GetComponent<TMP_Text>().enabled = false;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        bool cast = Physics.Raycast(playerHead.position, playerHead.forward, out hit, itemPickupDistance);
+
+        if (cast && hit.collider.gameObject.GetComponent<InteractableObjectTag>())  // hovering interactable object
+        {
+            if(targetedItem != null && targetedItem != hit.transform && targetedItem.GetComponent<Outline>() != null)   // exit hovering previous interactable object
+                targetedItem.GetComponent<Outline>().enabled = false;
+            targetedItem = hit.transform;      
+
+            if (hit.transform.GetComponent<Outline>() != null) // if interactable object has outline script
+            {
+                if (hit.collider.gameObject.GetComponent<PickableObjectTag>() != null)
+                {
+                    if (pickedItem == null)
+                        pickUpText.GetComponent<TMP_Text>().enabled = true;
+                }                   
+                else // text for any other interactable
+                    interactText.GetComponent<TMP_Text>().enabled = true;
+
+                hit.transform.GetComponent<Outline>().enabled = true;
+            }
+            else
+            {
+                if (hit.collider.gameObject.GetComponent<KeyHangerTag>() != null)
+                {
+                    // if interactable object is the key hanger and item in hand
+                    if (pickedItem != null)
+                        putItemBackText.GetComponent<TMP_Text>().enabled = true;
+                }
+                else // text for any other interactable
+                    interactText.GetComponent<TMP_Text>().enabled = true;
+            }
+                
+
+        }
+        else if (targetedItem != null)  
+        {
+            DisableTexts();         
+            if (targetedItem.GetComponent<Outline>() != null)
+                targetedItem.GetComponent<Outline>().enabled = false;
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.F))    
+        {
+            if (pickedItem != null) 
+            {
+                if (pickedItem.GetComponent<KeyTag>() != null)
+                {
+                    if (cast && hit.collider.gameObject.GetComponent<KeyHangerTag>() != null)
+                    {
+                        // put key back to hanger
+                        pickedItem.SetParent(initialParent);
+                        pickedItem.position = initialPosition;
+                        pickedItem.rotation = initialRotation;
+                        pickedItem = null;
+                        DisableTexts();
+                    }               
+                }
+                else
+                {
+                    // drop held item
+                    pickedItem.SetParent(null);
+                }
+            }
+            else if (cast)  // try picking item up
+            {
+                if (hit.collider.gameObject.GetComponent<PickableObjectTag>())
+                {
+
+                    pickedItem = hit.transform;
+                    initialPosition = pickedItem.position;      // save initial state
+                    initialRotation = pickedItem.rotation;
+                    initialParent = hit.transform.parent;
+
+                    GetHeldKeyNumber();
+
+                    pickedItem.SetParent(playerHand);         // ALTERNATIVE: setParent(camera) to follow camera
+                    pickedItem.position = playerHand.position;
+                    pickedItem.rotation = Quaternion.Euler(playerHand.rotation.eulerAngles + new Vector3(0f, -15f, 90f));   // align to hand + offset so object is facing forward
+                    DisableTexts();
+                }
+            }
+        }
+    }
+
+    public int GetHeldKeyNumber()
+    {
+        if(!pickedItem)
+            return -1;
+
+        GameObject obj = pickedItem.gameObject;
+        if(!obj.GetComponent<KeyTag>())
+            return -1;
+        
+        string objectName = obj.name;
+        int keyNumber;
+        if(int.TryParse(objectName[objectName.Length - 1].ToString(), out keyNumber))
+        {
+            Debug.Log(keyNumber);
+            return keyNumber;
+        }
+        else return -1;
+    }
+
+}

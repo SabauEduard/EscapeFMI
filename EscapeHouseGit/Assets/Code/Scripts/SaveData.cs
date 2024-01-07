3d499b86c930be4c7807f32db7a7b7d561207e4b
Bogdan Putineanu <94795061+197Bogdan@users.noreply.github.com>
2024-01-07 18:33:03 +0200

    Remove duplicate assets (#13)
    * Modified player movement and map
    
    * Salvare cladire modificata mare
    
    * Modified basement and others
    
    * added save game functionality
    
    f5 - save
    f9 - load
    f10 - delete save
    
    * implemented bookshelf animation + sound
    
    * changed secret room ladder to stairs
    
    * removed duplicates
    
    * s
    
    * refixed scene
    
    * s
    
    ---------
    
    Co-authored-by: MafteiValentin <maftei_valentin06@yahoo.com>

---
+++ EscapeHouseGit/Assets/Code/Scripts/SaveData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class SaveData
+{
+    public Vector3 playerPosition;
+    public Quaternion playerRotation;
+
+    public bool isDoorLocked;
+    public List<int> usedKeys;
+
+    public float gameVolume;
+
+}

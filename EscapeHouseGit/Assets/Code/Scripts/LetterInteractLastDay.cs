e35f4c971b1c89ab98125c5550628f2c67c4fd4b
MafteiValentin <maftei_valentin06@yahoo.com>
2024-01-12 01:53:11 +0200

    Third puzzle progress

---
+++ EscapeHouseGit/Assets/Code/Scripts/LetterInteractLastDay.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LetterInteractLastDay : MonoBehaviour
+{
+    public List<int> _usedKeys = new List<int>();
+
+    private PlayerInteractionsController _player = null;
+    private string _letterTagComponent;
+
+    public AudioSource playSound;
+    public AudioSource otherSound;
+    public GameObject textbox;
+    public GameObject otherText;
+
+    [SerializeField]
+    public float maxInteractDistance = 5.0f;
+    private void Start()
+    {
+        _player = FindObjectOfType<PlayerInteractionsController>();
+        _letterTagComponent = "LetterTagLastDay";
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        bool cast = Physics.Raycast(_player.playerHead.position, _player.playerHead.forward, out hit, maxInteractDistance);
+
+        if (Input.GetKeyDown(KeyCode.F) && cast && hit.collider.gameObject.GetComponent(_letterTagComponent))
+        {
+            otherSound.Stop();
+            otherText.SetActive(false);
+            playSound.Play();
+            StartCoroutine(PlaySubtitle());
+        }
+
+        IEnumerator PlaySubtitle()
+        {
+            textbox.SetActive(true);
+            yield return new WaitForSeconds(30);
+            textbox.SetActive(false);
+        }
+    }
+}

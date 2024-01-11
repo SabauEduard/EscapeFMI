c90da4052c0bc69a63b6032577e5c1335b481bae
Rogoza Raluca <raluca_rogoza@yahoo.com>
2024-01-11 10:38:34 +0200

    solved problem with camera when game is paused

--- EscapeHouseGit/Assets/Code/Scripts/PauseMenu.cs
+++ EscapeHouseGit/Assets/Code/Scripts/PauseMenu.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool paused = false;
     public GameObject PauseMenuCanvas;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,12 +38,14 @@ public class PauseMenu : MonoBehaviour
         PauseMenuCanvas.SetActive(true);
         Time.timeScale = 0f; // freeze time
         paused = true;
+        player.GetComponent<FirstPersonController>().enabled = false;
     }
     public void Play()
     {
         PauseMenuCanvas.SetActive(false);
         Time.timeScale = 1f;
         paused = false;
+        player.GetComponent<FirstPersonController>().enabled = true;
     }
 
     public void MainMenuButton()

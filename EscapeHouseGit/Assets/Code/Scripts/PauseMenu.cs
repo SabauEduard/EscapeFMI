dcbcbc59b63951968b5a4c328a5ca220053e2cf3
Raluca Rogoza <92272145+ralucarogoza@users.noreply.github.com>
2024-01-12 11:05:43 +0200

    Pause menu (#16)
    * added buttons and scroll area for journal
    
    * fixed pause menu
    
    * solved problem with camera when game is paused
    
    * disabled jump when pause menu is displayed
    
    * centered text for journal
    
    * duplicate main scene
    
    * added pause menu in main scene

---
+++ EscapeHouseGit/Assets/Code/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject PauseMenuCanvas;
+    public GameObject player;
+    
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Play();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+    }
+
+    void Stop()
+    {
+        PauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f; // freeze time
+        paused = true;
+        player.GetComponent<FirstPersonController>().enabled = false;
+        player.GetComponent<StarterAssetsInputs>().jump = false;
+    }
+    public void Play()
+    {
+        PauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+        player.GetComponent<FirstPersonController>().enabled = true;
+        player.GetComponent<StarterAssetsInputs>().jump = false;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); 
+    }
+}
